02e16bd28bf74ef75cabccddec553c7579e9e829
Joao Vitor Machado de Sá <joaovitormdesa@gmail.com>
2022-04-20 17:22:27 -0300

    Alterações no layout geral

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const ram = () =>{
-    const pers = document.getElementById("pers")
-    var id = pers.value;
     var char = []
-    const url = `https://rickandmortyapi.com/api/character/${id}`
+    const url = id => `https://rickandmortyapi.com/api/character/${id}`
     const div = document.getElementById("resposta")
 
-
-    char.push(fetch(url).then(response => response.json()))
+    for(let i=1; i<=820; i++){
+        char.push(fetch(url(i)).then(response => response.json()))
+    }
     
     Promise.all(char)
         .then(item =>{
@@ -14,20 +13,17 @@ const ram = () =>{
             var list = item.reduce((acumulattor, item) =>{
                 acumulattor += `
                 <div class="info">
-                        <div class="card-info">
+                    
                         <img class="card-img" alt="" src="${item.image}">
                         <br>
-                        <h2 class="card-title"> ${item.name}</h2> <br>
+                        <h2 class="card-name">Nome: ${item.name}</h2> <br>
                         <p class="card-subtitle"><strong>Genero:</strong> ${item.gender}</p>
                         <p class="card-subtitle"><strong>ID:</strong> ${item.id}</p>
-                    </div>
-                    <div class="card-info2">
                         <h2 class="card-title">Informações</h2> <br>
                         <p class="card-subtitle"><strong>Status:</strong> ${item.status}</p>
                         <p class="card-subtitle"><strong>Especie:</strong> ${item.species}</p>
                         <p class="card-subtitle"><strong>Origem:</strong> ${item.origin.name}</p>
-                        
-                    </div>
+                         
                 </div>
                 ` 
                 return acumulattor
@@ -37,4 +33,4 @@ const ram = () =>{
     
 }
 
-
+ram();
